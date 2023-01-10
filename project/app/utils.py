9ac2d9b7db252181e9526cead97c432d4309bbf4
Diyor <115725709+DIYballPLAYER@users.noreply.github.com>
2023-01-10 14:53:30 +0500

    Update utils.py

--- project/app/utils.py
+++ project/app/utils.py
@@ -1,11 +1,12 @@
-import requests
+import requests as requests
 from bs4 import BeautifulSoup
 
-from project.app.config import SEARCH_URL, YANDEX_URL
+from project.app.config import SEARCH_URL
 
 
 def convert_to_soup(content: str) -> BeautifulSoup:
     return BeautifulSoup(requests.get(SEARCH_URL + content).text, "html.parser")
+
 
 
 def image_link_parser(page: BeautifulSoup) -> list:
@@ -15,12 +16,20 @@ def image_link_parser(page: BeautifulSoup) -> list:
         link_ = link.get("data-src")
         if link_ is not None:
             links_container.append(link_)
-    return links_container if links_container is not None else print(' Фoтo не найдено:(')
+    print(links_container)
+    return links_container
 
 
-def image_uploader(url: str, token: str, theme: str) -> None:
-    response = requests.post(YANDEX_URL,
-                             headers={"Authorization": f"OAuth {token}"},
-                             params={"url": f"{url}",
-                                     "path": f"/{theme}.jpg"})
+def image_uploader(token: str) -> None:
+    theme = input("Напишите тему фото которых хотите установить(на английском): ")
+    page = convert_to_soup(theme)
+    link_container = image_link_parser(page)
+    print(link_container)
+    for link in link_container:
+        response = requests.post("https://cloud-api.yandex.net/v1/disk/resources/upload",
+                                 headers={"Authorization": f"OAuth {token}"},
+                                 params={"url": f"{link}",
+                                         "path": f"/{theme}.jpg"})
+        print(response.status_code)
+        print(response.url)
 
